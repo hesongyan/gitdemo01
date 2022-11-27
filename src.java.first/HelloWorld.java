3c18cb8d0d3f4ca59bfc59c9070acea6a0a286ad
Yanhs <yhs123456@foxmail.com>
2022-11-27 10:26:27 +0800

    HelloWorld

---
+++ src.java.first/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Yanhs
+ * @create 2022-11-27-10:23
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+    }
+}
